168016a6cbd31cd3db01cefe26d33a75c87dd96e
Harshit Vishwakarma <130080307+HarshitViskrma@users.noreply.github.com>
2025-01-11 17:23:16 +0530

    color palette changes

--- footer.php
+++ footer.php
@@ -1,30 +1,30 @@
 		<!-- Footer -->
 		<footer id="footer" class="section">
 
-			<!-- container -->
-			<div class="container">
+		    <!-- container -->
+		    <div class="container">
 
-				<!-- row -->
-				<div class="row">
+		        <!-- row -->
+		        <div class="row">
 
-					<!-- footer logo -->
-					<div class="col-md-6">
-						<div class="footer-logo">
-							<a class="logo" style="font-size: 30px;" href="main.php">AspireBridge</a>
-						</div>
-					</div>
-					<!-- footer logo -->
+		            <!-- footer logo -->
+		            <div class="col-md-6">
+		                <div class="footer-logo">
+		                    <a class="logo" style="font-size: 30px;" href="main.php">AspireBridge</a>
+		                </div>
+		            </div>
+		            <!-- footer logo -->
 
 
-				</div>
-				<!-- /row -->
+		        </div>
+		        <!-- /row -->
 
-				<!-- row -->
-				<div id="bottom-footer" class="row">
+		        <!-- row -->
+		        <div id="bottom-footer" class="row">
 
-					<!-- social -->
-					<div class="col-md-4 col-md-push-8">
-						<!--<ul class="footer-social">
+		            <!-- social -->
+		            <div class="col-md-4 col-md-push-8">
+		                <!--<ul class="footer-social">
 							<li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
 							<li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
 							<li><a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a></li>
@@ -32,28 +32,30 @@
 							<li><a href="#" class="youtube"><i class="fa fa-youtube"></i></a></li>
 							<li><a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
 						</ul>-->
-					</div>
-					<!-- /social -->
+		            </div>
+		            <!-- /social -->
 
-					<!-- copyright -->
-					<div class="col-md-8 col-md-pull-4">
-						<div class="footer-copyright">
-							<span>&copy; Copyright 2021. All Rights Reserved. </span>
-						</div>
-					</div>
-					<!-- /copyright -->
+		            <!-- copyright -->
+		            <div class="col-md-8 col-md-pull-4">
+		                <div class="footer-copyright">
+		                    <span>&copy; Copyright 2025. All Rights Reserved. </span>
+		                </div>
+		            </div>
+		            <!-- /copyright -->
 
-				</div>
-				<!-- row -->
+		        </div>
+		        <!-- row -->
 
-			</div>
-			<!-- /container -->
+		    </div>
+		    <!-- /container -->
 
 		</footer>
 		<!-- /Footer -->
 
 		<!-- preloader -->
-		<div id='preloader'><div class='preloader'></div></div>
+		<div id='preloader'>
+		    <div class='preloader'></div>
+		</div>
 		<!-- /preloader -->
 
 
@@ -62,4 +64,4 @@
 		<script type="text/javascript" src="js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="js/main.js"></script>
 
-    </body>
+		</body>
